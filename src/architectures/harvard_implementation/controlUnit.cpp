927d3aab4a4f4a7eb63f5f14a1ea64f4c97e6a05
benjaminhelyer <91896754+BenjaminHelyer@users.noreply.github.com>
2022-04-20 17:09:50 -0500

    added structure of switch for determining which instruction was handed to the control unit

--- src/architectures/harvard_implementation/controlUnit.cpp
+++ src/architectures/harvard_implementation/controlUnit.cpp
@@ -54,24 +54,142 @@ void ControlUnit::execute_instruction(RiscvInstruction ctrlInstruction) {
     int opcode = 0;
     opcode = ctrlInstruction.get_opcode();
 
+    int extend_op = 0;
+
     bool full_instr[32] = { };
     ctrlInstruction.copy_contents(full_instr);
 
     switch (opcode) {
         case 3:
             // opcode is for some I type instruction
+            extend_op = get_opcode_extend(full_instr);
+            switch (extend_op) {
+                case 0:
+                    // I lb instruction
+                case 1:
+                    // I lh instruction
+                case 2:
+                    // I lw instruction
+                case 4:
+                    // I lbu instruction
+                case 5:
+                    // I lhu instruction
+                default:
+                    throw std::invalid_argument("extended opcode not recognized");
+                    return;
+            }
         case 19:
             // opcode is for some I type instruction
+            extend_op = get_opcode_extend(full_instr);
+            switch (extend_op) {
+                case 0:
+                    // I addi instruction
+                case 2:
+                    // I slti instruction
+                case 3:
+                    // I sltiu instruction
+                case 4:
+                    // I xori instruction
+                case 6:
+                    // I ori instruction
+                case 7:
+                    // I andi instruction
+                default:
+                    throw std::invalid_argument("extended opcode not recognized");
+                    return;
+            }
         case 35:
             // opcode is for some S type instruction
+            extend_op = get_opcode_extend(full_instr);
+            switch (extend_op) {
+                case 0:
+                    // S sb instruction
+                case 1:
+                    // S sh instruction
+                case 2:
+                    // S sw instruction
+                default:
+                    throw std::invalid_argument("extended opcode not recognized");
+                    return;
+            }
         case 51:
             // opcode is for some R type instruction
+            extend_op = get_opcode_extend(full_instr);
+            switch (extend_op) {
+                case 0:
+                    // NEED TO DETERMINE IF ADD OR SUB AFTER THIS
+                case 1:
+                    // R sll instruction
+                case 2:
+                    // R slt instruction
+                case 3:
+                    // R sltu instruction
+                case 4:
+                    // R xor instruction
+                case 5:
+                    // NEED TO DETERMINE IF SRA OR SRL AFTER THIS
+                case 6:
+                    // R or instruction
+                case 7:
+                    // R and instruction
+                default:
+                    throw std::invalid_argument("extended opcode not recognized");
+                    return;
+            }
         case 15:
             // opcode is for some I type instruction
+            extend_op = get_opcode_extend(full_instr);
+            switch (extend_op) {
+                case 0:
+                    // I fence instruction
+                case 1:
+                    // I fence.i instruction
+                default:
+                    throw std::invalid_argument("extended opcode not recognized");
+                    return;
+            }
         case 115:
             // opcode is for some I type instruction
+            extend_op = get_opcode_extend(full_instr);
+            switch (extend_op) {
+                case 0:
+                    // NEED TO DETERMINE DIFFERENCE BETWEEN ECALL AND EBREAK
+                case 1:
+                    // I csrrw instruction
+                case 2:
+                    // I csrrs instruction
+                case 3:
+                    // I csrrc instruction
+                case 5:
+                    // I csrrwi instruction
+                case 6:
+                    // I csrrsi instruction
+                case 7:
+                    // I csrrci instruction
+                default:
+                    throw std::invalid_argument("extended opcode not recognized");
+                    return;
+            }
         case 99:
             // opcode is for some B type instruction
+            extend_op = get_opcode_extend(full_instr);
+            switch (extend_op) {
+                case 0:
+                    // B beq instruction
+                case 1:
+                    // B bne instruction
+                case 4:
+                    // B blt instruction
+                case 5:
+                    // B bge instruction
+                case 6:
+                    // B bltu instruction
+                case 7:
+                    // B bgeu instruction
+                default:
+                    throw std::invalid_argument("extended opcode not recognized");
+                    return;
+            }
         case 239:
             // opcode is for J jal
             // return after jump and do not increment program counter
